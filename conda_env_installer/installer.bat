0b03b40dd95b318b4382eea7f72284e92ecaa6ad
GunnerStone <gunnerinlv@gmail.com>
2022-03-22 20:33:54 -0700

    Easy Launch & Dependency Installer

--- conda_env_installer/installer.bat
+++ conda_env_installer/installer.bat
@@ -5,17 +5,5 @@
 @echo off
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
-:: Starts the conda environment needed to run the bot
-call activate pySwitchBots
-
-:: Starts the bot with python
-
-echo [32mReposition the terminal window out of the way of the capture utility ^& MaxAimDI Plugin[0m
-
-set /p DUMMY=[33mHit ENTER to start the bot...[0m
-cls
-color 0F
-python BDSP-Scripts/script_runner.py
-
-:: Changes the window position to the top right corner
-WINDOW[bottom]
+:: Installs the conda environment needed to run the bot
+call conda env create -f environment.yml
