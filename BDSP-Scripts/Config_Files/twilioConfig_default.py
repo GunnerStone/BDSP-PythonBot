424e505b53872c1aa4afad354e710db8dbe49bf9
GunnerStone <gunnerinlv@gmail.com>
2022-03-28 15:28:25 -0700

    button mapping fixes & absent twilio bugfix

---
+++ BDSP-Scripts/Config_Files/twilioConfig_default.py
@@ -0,0 +1,4 @@
+to_phone_number = '<your phone number>'
+from_phone_number = '<twilio phone number>'
+account_sid = '<twilio account sid>'
+auth_token = '<twilio auth token>'
--- BDSP-Scripts/utils/pokeTwilio.py
+++ BDSP-Scripts/utils/pokeTwilio.py
@@ -3,7 +3,10 @@ from twilio.rest import Client
 #import twilioConfig from one folder up and inside Config_Files folder
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Config_Files import twilioConfig
+try:
+    from Config_Files import twilioConfig
+except ImportError:
+     from Config_Files import twilioConfig_default as twilioConfig
 
 #   This file holds a function to call a player with a statement. In this case, finding a shiny.
 #
@@ -16,16 +19,19 @@ from Config_Files import twilioConfig
 
 def found_shiny_text(found_pokemon = '', to_num = twilioConfig.to_phone_number, from_num = twilioConfig.from_phone_number): 
     # This function calls a user and says the message "You Found a Shiny!". Usage: found_shiny_call(to_num, from_num). Num format: Country Code + Area Code + Number (example: '+12223333333')
-    sentence = 'You Found a Shiny ' + found_pokemon
-    formatted = '<Response><Say>' + sentence + '</Say></Response>'
-    account_sid = twilioConfig.account_sid
-    auth_token = twilioConfig.auth_token
-    client = Client(account_sid, auth_token)
+    try:
+        sentence = 'You Found a Shiny ' + found_pokemon
+        formatted = '<Response><Say>' + sentence + '</Say></Response>'
+        account_sid = twilioConfig.account_sid
+        auth_token = twilioConfig.auth_token
+        client = Client(account_sid, auth_token)
 
-    message = client.messages.create(
-        body=sentence,
-        from_=from_num,
-        to=to_num)
+        message = client.messages.create(
+            body=sentence,
+            from_=from_num,
+            to=to_num)
 
-    # client.calls.create(twiml=formatted, to = to_num, from_ = from_num)
-    print("Texting Phone Number: "+str(to_num))
+        # client.calls.create(twiml=formatted, to = to_num, from_ = from_num)
+        print("Texting Phone Number: "+str(to_num))
+    except:
+        print("Twilio is not configured properly. Please check your twilioConfig_default.py file.")
