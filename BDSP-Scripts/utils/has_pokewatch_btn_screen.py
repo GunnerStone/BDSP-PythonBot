197964dcb7313d1150dae062b3dde17b64571584
GunnerStone <gunnerinlv@gmail.com>
2022-03-30 13:44:48 -0700

    bgrtorgb removed

--- BDSP-Scripts/utils/has_pokewatch_btn_screen.py
+++ BDSP-Scripts/utils/has_pokewatch_btn_screen.py
@@ -10,8 +10,6 @@ def has_pokewatch_btn_screen(driver):
     img = cv2.resize(img, (1280, 720))
 
     img = img[150:250,1140:1180] # pokewatch screen
-    # invert the colors from bgr to rgb
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     if driver.match_image(img, "BDSP-Scripts/assets/pokewatch_btn.png", threshold=0.1):
         return True
     else:
