db77b08e023ce3942cfff3c6d3e9a32f0d63f3dc
Lee <leecbryant@outlook.com>
2022-03-29 22:01:09 -0700

    fixed dodo brain if statement

--- BDSP-Scripts/utils/has_white_screen.py
+++ BDSP-Scripts/utils/has_white_screen.py
@@ -7,9 +7,7 @@ def has_white_screen(driver):
     # convert to opencv
     img = np.array(img)
     img = cv2.resize(img, (1280, 720))
-    img = img[0:600,605:1168]
+    img = img[50:550, 655:1118]
+    
     # if a large majority of the image is white, return true
-    if cv2.mean(img)[0] > 240:
-        return True
-    else:
-        return False
+    return cv2.mean(img)[0] > 240
