b1105d096472330a62eb16f463da49ecfbba6523
GunnerStone <gunnerinlv@gmail.com>
2022-03-31 01:20:15 -0700

    timing fixes

--- BDSP-Scripts/scripts/vs_seeker_money_farmer.py
+++ BDSP-Scripts/scripts/vs_seeker_money_farmer.py
@@ -27,6 +27,8 @@ def run(driver, controller):
     # enable running by pressing B down
     pydirectinput.keyDown(controller['Key_B'])
     time.sleep(0.1)
+    logging.debug("Navigating to old couple")
+
     """ Navigate out of hearthstone"""
     driver.hold_key(controller['Key_Dpad_Down'].lower(),2.1).wait(0.2)
     driver.hold_key(controller['Key_Dpad_Right'].lower(),2.9).wait(0.2)
@@ -50,11 +52,15 @@ def run(driver, controller):
     driver.hold_key(controller['Key_Dpad_Down'].lower(),2.9).wait(0.2)
 
     def charge_vs_seeker():
+        logging.debug("Charging VS Seeker")
+
         for i in range(0,6):
             driver.hold_key(controller['Key_Dpad_Left'].lower(),1.8)
             driver.hold_key(controller['Key_Dpad_Right'].lower(),1.8).wait(0.2)
 
     def use_vs_seeker():
+        logging.debug("Using VS Seeker")
+
         driver.hold_key(controller['Key_Start'].lower(),0.2).wait(0.5)
         driver.hold_key(controller['Key_Dpad_Down'].lower(),0.2).wait(0.5)
 
@@ -67,9 +73,11 @@ def run(driver, controller):
         # take a crop of the inner portion of the screen
         img = img[120:600,300:900]
 
-        if driver.match_image(img, "BDSP-Scripts/assets/vs_seeker_alert.png", threshold=0.05):
+        if driver.match_image(img, "BDSP-Scripts/assets/vs_seeker_alert.png", threshold=0.01):
+            logging.debug("VS Seeker Alert found on screen")
             return True
         else:
+            logging.debug("VS Seeker Alert not found on screen")
             return False
 
     def check_for_fight():
@@ -78,6 +86,8 @@ def run(driver, controller):
         use_vs_seeker()
         found_fight_flag = False
         # check for vs seeker alert until pokewatch reappears
+        logging.debug("Scanning for VS Seeker Alert on screen")
+
         while not utils.has_pokewatch_btn_screen(driver):
             if locate_vs_seeker_alert_on_screen():
                 found_fight_flag = True
@@ -88,22 +98,31 @@ def run(driver, controller):
 
     num_PP = 30 # how many fights to do
     while num_PP > 0:
+        logging.debug("Starting VS Seeker Loop with PP: {}".format(num_PP))
         if check_for_fight():
             num_PP -= 1
             # walk to the left and fight the vs seeker
             # release the b button
             pydirectinput.keyUp(controller['Key_B'])
             time.sleep(0.1)
-            driver.hold_key(controller['Key_Dpad_Left'].lower(),0.6).wait(0.2)
+            logging.debug("Walking to old ldady")
+
+            driver.hold_key(controller['Key_Dpad_Left'].lower(),0.1).wait(0.2)
+            driver.hold_key(controller['Key_Dpad_Left'].lower(),0.1).wait(0.2)
+            driver.hold_key(controller['Key_Dpad_Left'].lower(),0.1).wait(0.2)
             driver.hold_key(controller['Key_Dpad_Down'].lower(),0.1).wait(0.2)
+
+            logging.debug("Pressing A until pokewatch disappears")
             # press A until the pokewatch disappears
             while utils.has_pokewatch_btn_screen(driver):
                 driver.hold_key(controller['Key_A'].lower(),0.2).wait(0.2)
 
+            logging.debug("Inside battle; Pressing A until the pokewatch reappears")
             # press A until the pokewatch reappears
             while not utils.has_pokewatch_btn_screen(driver):
                 driver.hold_key(controller['Key_A'].lower(),0.2).wait(0.2)
-            
+
+            logging.debug("Pressing B to remove extra dialogue with old lady")            
             # remove dialogue with the vs seeker
             driver.hold_key(controller['Key_B'].lower(),0.2).wait(0.2)
             driver.hold_key(controller['Key_B'].lower(),0.2).wait(0.2)
